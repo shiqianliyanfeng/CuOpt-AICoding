38acaf62b6155913fb793722952cca67ee063173
shiqian01 <shiqian01@100.me>
2025-08-01 17:48:44 +0800

    update

--- solve/vrp_batch_solver.py
+++ solve/vrp_batch_solver.py
@@ -411,6 +411,6 @@ class VRPMIPBatchSolver:
 if __name__ == "__main__":
     current_dir = os.getcwd()
     print(current_dir)
-    solver = VRPMIPBatchSolver("./data/vrp_dataset_100.json")
+    solver = VRPMIPBatchSolver("../data/vrp_dataset_100.json")
     solver.batch_solve()
     solver.show_stats()
